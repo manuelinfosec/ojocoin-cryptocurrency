5e74d656698f99d1646b4987122a1911effa67d2
Manuel InfoSec <manuelinfosec@gmail.com>
2022-09-07 22:19:16 +0100

    Update ojocoin/storage.py

--- ojocoin/storage.py
+++ ojocoin/storage.py
@@ -4,25 +4,25 @@ def register_node(self, address):
 
 	#consensus algo
 def resolveConflicts(self):
-	neighbors = self.nodes;
-	newChain = None;
+	neighbors = self.nodes
+	newChain = None
 
-	maxLength = len(self.chain);
+	maxLength = len(self.chain)
 
 	for node in neighbors:
-		response = requests.get(f'http://{node}/chain');
+		response = requests.get(f'http://{node}/chain')
 
 		if response.status_code == 200:
-			length = response.json()['length'];
-			chain = response.json()['chain'];
+			length = response.json()['length']
+			chain = response.json()['chain']
 
 			if length > maxLength and self.isValidChain():
-				maxLength = length;
-				newChain = chain;
+				maxLength = length
+				newChain = chain
 
 	if newChain:
-		self.chain = self.chainJSONdecode(newChain);
-		print(self.chain);
-		return True;
+		self.chain = self.chainJSONdecode(newChain)
+		print(self.chain)
+		return True
 
-	return False;
+	return False
